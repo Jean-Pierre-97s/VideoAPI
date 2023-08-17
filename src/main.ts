346abc26363fa85e6a239ca0a04b6640b1738de6
Jean <jean_pierre_97@outlook.com>
2023-08-17 16:22:14 -0300

    feat: added validator

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from 'exception-filters/prisma.exception-filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new PrismaExceptionFilter());
+
+  app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
 
   await app.listen(3000);
 }
